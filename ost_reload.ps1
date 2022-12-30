a6bf933994fd6758cd06030078a9f82719580a57
Tanner Mills <tanner.mills2000@gmail.com>
2022-12-29 17:24:01 -0700

    Unworking second iteration

--- ost_reload.ps1
+++ ost_reload.ps1
@@ -1,13 +1,70 @@
-$usern = [System.Environment]::UserName
 
-Stop-Process -Name "Outlook" -Force
+class OstReload {
+    static [string[]] $exemptUsers = "Administrator", "LogMeInRemoteUser", "Public", "gblackburnadmin"
 
-cd C:\Users\$usern\Appdata\Local\Microsoft\Outlook
+    hidden [string]$machineName 
+    hidden [string] $currentUser 
+    hidden [string[]]$localUsers
+    hidden [string[]]$userList
 
-Get-ChildItem -Filter "OLD - $usern*" | Remove-Item
+    ## Constructor ##
+    Device() {
+        $this.currentUser = ((Get-CimInstance -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+        $this.machineName = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+        $this.localUsers = @((Get-ChildItem C:\Users).Name)
+    }
+   
+    static [void] prompt() {
+	    Write-Host "Is this the correct user? Enter y/n`n"
+	    $bool = Read-Host
+        
+	    if (($bool -eq 'y') -or ($bool -eq 'yes')) {
+		    Write-Host 
+	    }
+	    elseif (($bool -eq 'n') -or ($bool -eq 'no')) {
+    		
+	    }
+	    else {
+		    Write-Host "Oops, wrong option. Try again."
+            [OstReload]::prompt()
+	    }
+    }
 
-Start-Sleep -m 1000
+    static [int] getSelection([int[]]$options) {
+        return 0
+    }
 
-Get-ChildItem -Filter "$usern*" | Rename-Item -NewName {$_.Name -replace "^", "OLD - "}
 
-Start-Process Outlook.exe
+    [void] start() {
+        $this.setCurrentUser()
+    }
+
+
+    hidden [void] setCurrentUser() {
+        Write-Host "Current user set to:" ($this.getCurrentUser()).toUpper()
+        [OstReload]::prompt()
+
+    }
+
+    hidden [string] getCurrentUser() {
+	    return $this.currentUser
+    }
+    
+    hidden [string[]] getLocalUsers($currentUser) {
+        $this.localUsers = @((Get-ChildItem C:\Users).Name)
+
+        Write-Host
+
+        for ($i = 0; $i -lt $this.localUsers.Length; $i++) {
+            if ($this.localUsers[$i] -contains [OstReload]::exemptUsers -or $this.currentUser) {
+                
+            }
+        }
+    }
+}
+
+[OstReload]$session = [OstReload]::new()
+$session.setCurrUser()
+
+
+## $session.start()
